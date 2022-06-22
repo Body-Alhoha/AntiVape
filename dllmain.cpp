bb8e14340b984153c0f926aaf432cd216d12b70c
Body-Alhoha <ilyas.laassel2@gmail.com>
2022-06-22 14:58:07 +0200

    vape lite injection prevented

--- dllmain.cpp
+++ dllmain.cpp
@@ -4,6 +4,7 @@
 #include "JNI/jvmti.h"
 #include <iostream>
 #include "md5.h"
+#include "hook/MinHook.h"
 JavaVM* jvm;
 JNIEnv* jenv;
 jvmtiEnv* jvmti;
@@ -16,10 +17,42 @@ unsigned char ripvape[] = {
 static void JNICALL ClassFileLoadHook(jvmtiEnv* jvmti, JNIEnv* jni, jclass class_being_redefined, jobject loader, const char* name, jobject protection_domain, jint class_data_len, const unsigned char* class_data, jint* new_class_data_len, unsigned char** new_class_data);
 
 FILE* pFile = nullptr;
+
+typedef jint (JNICALL * getCreatedJavaVMS)(JavaVM **, jsize, jsize *);
+getCreatedJavaVMS real = NULL;
+jint WINAPI GetCreatedJavaVMs(JavaVM** vm, jsize s, jsize* ss)
+{
+    std::cout << "Vape injection prevented" << std::endl;
+    return 0;
+    //return real(vm, s, ss);
+}
+
+void HookCreatedJavaVMS() {
+    HMODULE jvmDll = GetModuleHandleA("jvm.dll");
+    FARPROC javavms = GetProcAddress(jvmDll, "JNI_GetCreatedJavaVMs");
+    printf("Address of JNI_GetCreatedJavaVMs :%p\n", javavms);
+    if (MH_Initialize() != MH_OK)
+    {
+        std::cout << "failed to initialize MinHok" << std::endl;
+        return;
+    }
+    std::cout << "MinHook initalized" << std::endl;
+    getCreatedJavaVMS temp = (getCreatedJavaVMS)reinterpret_cast<LPVOID>((javavms));
+    MH_CreateHook(temp, &GetCreatedJavaVMs, (void**)&real);
+    MH_EnableHook(temp);
+    std::cout << "Hook done" << std::endl;
+
+   
+    
+}
+
+
+
 void init() {
     AllocConsole();
     freopen_s(&pFile, "CONOUT$", "w", stdout);
     freopen_s(&pFile, "CONIN$", "r", stdin);
+   
     jsize vmCount;
     if (JNI_GetCreatedJavaVMs(&jvm, 1, &vmCount) != JNI_OK || vmCount == 0) {
         std::cout << "JVM not found" << std::endl;
@@ -43,17 +76,24 @@ void init() {
     }
 
     std::cout << "Loaded successfuly, Hooking..." << std::endl;
+
+    HookCreatedJavaVMS();
+
+    //incase it bypasses attach current thread
+
     static jvmtiCapabilities capa;
     (void)memset(&capa, 0, sizeof(jvmtiCapabilities));
     capa.can_retransform_classes = 1;
     jvmti->AddCapabilities(&capa);
     jvmtiEventCallbacks callbacks = { 0 };
     callbacks.ClassFileLoadHook = ClassFileLoadHook;
+
     jvmti->SetEventCallbacks(&callbacks, sizeof(callbacks));
     error = jvmti->SetEventNotificationMode(JVMTI_ENABLE,
         JVMTI_EVENT_CLASS_FILE_LOAD_HOOK,
         (jthread)NULL);
-
+    
+   
 
 }
 unsigned char* antivape = NULL;
@@ -62,7 +102,7 @@ ClassFileLoadHook(jvmtiEnv* jvmti, JNIEnv* jni, jclass class_being_redefined, jo
     if (name != NULL) {
         std::string sName(name);
         if (sName.starts_with("a/")) {
-            std::cout << "vape injection prevented" << std::endl;
+            std::cout << "vape v4 injection prevented" << std::endl;
             ZeroMemory(antivape, sizeof(antivape));
             jvmti->Allocate(((jlong)(sizeof(ripvape))), &antivape);
             memcpy(antivape, ripvape, (int)sizeof(ripvape));
